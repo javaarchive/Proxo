f2bdc19ffaebf70954e7bea4e75ef9dacd6e24a8
Glitch (demo-github-proxo) <none>
2020-02-08 06:43:18 +0000

    Github Mirroring

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 port = 3000 # To be changed to get the env variable
 origins = {
-  "main":"http://glitch.com" # Automatically selected
+  "main":"http://github.com" # Automatically selected
 }
 forward_headers = ["User-Agent","Accept","Accept-Language","Accept-Encoding"] # Headers you want to forwards
 blocked_paths = ["/sw.js","/___glitch_loading_status___","/favicon.ico"]
